284ac9f19ef771e746a085baccbaa313ebad94ac
Green15-coder <malik3.co527@gmail.com>
2026-05-15 12:25:58 -0500

    Create SkipToContent.tsx

---
+++ src/components/layout/SkipToContent.tsx
@@ -0,0 +1,10 @@
+export function SkipToContent() {
+  return (
+    <a
+      href="#main-content"
+      className="sr-only focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50 focus:rounded-md focus:bg-slate-900 focus:px-3 focus:py-2 focus:text-white"
+    >
+      Skip to content
+    </a>
+  );
+}
